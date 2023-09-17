df105c0a2045e16a51ea6306dd4c28276cd1e03c
Eric <ericmuthuipatch22@gmail.com>
2023-09-18 00:10:17 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,12 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
+        
 
     def load_json(self):
-        pass
+        response_body = json.loads(self.get_response_body())
+        return response_body
+        
+   
